890936f2943b935719bdff0f874b0fa6863613a3
Anatoliy Siryi <hmG3@users.noreply.github.com>
2020-01-11 20:01:15 +0200

    Primary reverse engineering

---
+++ AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TALibCore")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TicTacTec")]
+[assembly: AssemblyProduct("TALibCore")]
+[assembly: AssemblyCopyright("Copyright © TicTacTec 2006")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyDelaySign(false)]
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: AssemblyVersion("1.0.7312.40736")]
