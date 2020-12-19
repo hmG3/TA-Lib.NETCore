51ebc916da91c5328b74f54afbe447c76606d977
Anatoliy Siryi <hmG3@users.noreply.github.com>
2020-12-19 14:16:18 +0200

    Add basic set of tests for functions

---
+++ tests/TALib.NETCore.Tests/Models/JsonExtensions.cs
@@ -0,0 +1,61 @@
+using System;
+using System.Buffers;
+using System.Text.Json;
+
+namespace TALib.NETCore.Tests.Models
+{
+    public static class JsonExtensions
+    {
+        public static T ToObject<T>(this JsonElement element, JsonSerializerOptions? options = null)
+        {
+            var bufferWriter = new ArrayBufferWriter<byte>();
+            using (var writer = new Utf8JsonWriter(bufferWriter))
+            {
+                element.WriteTo(writer);
+            }
+
+            return JsonSerializer.Deserialize<T>(bufferWriter.WrittenSpan, options);
+        }
+
+        public static T ToObject<T>(this JsonDocument document, JsonSerializerOptions? options = null)
+        {
+            if (document == null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            return document.RootElement.ToObject<T>(options);
+        }
+
+        public static object ToObject(this JsonElement element, Type returnType, JsonSerializerOptions? options = null)
+        {
+            var bufferWriter = new ArrayBufferWriter<byte>();
+            using (var writer = new Utf8JsonWriter(bufferWriter))
+            {
+                element.WriteTo(writer);
+            }
+
+            try
+            {
+                JsonSerializer.Deserialize(bufferWriter.WrittenSpan, returnType, options);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            return JsonSerializer.Deserialize(bufferWriter.WrittenSpan, returnType, options);
+        }
+
+        public static object ToObject(this JsonDocument document, Type returnType, JsonSerializerOptions? options = null)
+        {
+            if (document == null)
+            {
+                throw new ArgumentNullException(nameof(document));
+            }
+
+            return document.RootElement.ToObject(returnType, options);
+        }
+    }
+}
---
+++ tests/TALib.NETCore.Tests/Models/TestDataModel.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace TALib.NETCore.Tests.Models
+{
+    public class TestDataModel<T> where T : struct
+    {
+        public string Name { get; set; } = null!;
+
+        public T[][] Inputs { get; set; } = null!;
+
+        public T[] Options { get; set; } = Array.Empty<T>();
+
+        public T[][] Outputs { get; set; } = null!;
+
+        public bool Skip { get; set; }
+
+        public override string ToString() => Name;
+    }
+}
